872dd02a8934fa9e96bc5d38b69ca2e90585a5cf
bouharri <bouharri@nmu.edu>
2023-11-09 15:35:59 -0500

    Squashed 'apps/childhood-cancer/' content from commit 7284ac5
    git-subtree-dir: apps/childhood-cancer
    git-subtree-split: 7284ac52ad4f8a5c0b29fc513fc3506d23f65cb7

---
+++ README.md
@@ -0,0 +1,10 @@
+# U.P. Lions Serve: Childhood Cancer Website
+
+A portal to all other UPLS organizations
+
+### TO-DO
+
+◻️ create framework skeleton that this inherits
+◻️ fix env-based url linking
+◻️ fix `@astrojs/node` (missing feature map)  
+◻️ Implement CMS fetches
---
+++ src/components/Heading.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import Icon from "./Icon.svelte";
+
+  export let size: number;
+
+  export let color: "orange" | "orange-light" | "navy" | "white" = "navy";
+  let textColor: string;
+  switch (color) {
+    case "orange":
+      textColor = "text-orange";
+      break;
+    case "orange-light":
+      textColor = "text-orange-light";
+      break;
+    case "navy":
+      textColor = "text-navy";
+      break;
+    case "white":
+      textColor = "text-white";
+      break;
+  }
+
+  export let align: "left" | "center" | "right" = "left";
+  let textAlign: string;
+  switch (align) {
+    case "left":
+      textAlign = "justify-start";
+      break;
+    case "center":
+      textAlign = "justify-center";
+      break;
+    case "right":
+      textAlign = "justify-end";
+      break;
+  }
+
+  export let icon: string | null = null;
+</script>
+
+<span class="flex items-center {textAlign} gap-4">
+  {#if icon}
+    <Icon id={icon} {color} styling="w-8 h-8" />
+  {/if}
+
+  {#if size == 1}
+    <h1 class={textColor}><slot /></h1>
+  {:else if size == 2}
+    <h2><slot /></h2>
+  {:else if size == 3}
+    <h3><slot /></h3>
+  {:else if size == 4}
+    <h4><slot /></h4>
+  {:else if size == 6}
+    <h6><slot /></h6>
+  {/if}
+</span>
