7d9dc4332b1b78e7358fcc42b9ec7abc8af44151
Harrison Bouche <harrison@sm.media>
2024-03-10 14:27:56 -0400

    feat: added prettier, GAnalytics & partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config"
+import partytown from "@astrojs/partytown"
 import svelte from "@astrojs/svelte"
 import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), tailwind()],
+	integrations: [
+		svelte(),
+		tailwind(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
 	site: "https://uplionsserve.org",
 	output: "static",
 })
